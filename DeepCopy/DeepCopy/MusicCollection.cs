a7233d71175bd16c7b4c04733043f7a56812fa42
Mani Ramachandran <manig16@gmail.com>
2019-04-07 15:54:09 -0400

    Add files via upload

---
+++ DeepCopy/DeepCopy/MusicCollection.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace DeepCopy
+{
+    [Serializable()]
+    public class MusicCollection : ICloneable
+    {
+        public string DeviceName { get; set; }
+        public string DeviceType { get; set; }
+        public List<MusicFile> MusicFiles { get; set; }
+
+        [Obsolete("Thic c'tor  is meant for the XML Serialization library.")]
+        public MusicCollection()
+        {
+            MusicFiles = new List<MusicFile>();
+        }
+
+        public MusicCollection(string deviceName, string deviceType) : base() {
+            DeviceName = deviceName;
+            DeviceType = deviceType;
+            MusicFiles = new List<MusicFile>();
+        }
+
+        public void AddMusic( MusicFile file)
+        {
+            MusicFiles.Add(file);
+        }
+
+        public object Clone() {
+            MemoryStream ms = new MemoryStream();
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(ms, this);
+            ms.Position = 0;
+            object obj = bf.Deserialize(ms);
+            ms.Close();
+            return obj;
+
+          //  return this.MemberwiseClone(); // For Shallow Copy
+        }
+    }
+
+    [Serializable]
+    public class MusicFile
+    {
+        public string Name { get; set; }
+        public General General { get; set; }
+        public Description Description { get; set; }
+
+        [Obsolete("Do not use this c'tor")]
+        public MusicFile()
+        {
+            General = new General();
+            Description = new Description();
+        }
+
+        public MusicFile(string name)
+        {
+            Name = name;
+            General = new General();
+            Description = new Description();
+        }
+    }
+
+    [Serializable]
+    public class General
+    {
+        public string TypeOfFile { get; set; }
+        public string OpensWith { get; set; }
+        public string Location { get; set; }
+        public long Size { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Modified { get; set; }
+        public DateTime Accessed { get; set; }
+        public bool ReadOnly { get; set; }
+        public bool Hidden { get; set; }
+
+        [Obsolete("Do not use this c'tor")]
+        public General()
+        {
+        }
+
+        public General(string typeOfFile)
+        {
+            TypeOfFile = typeOfFile;
+        }
+    }
+
+    [Serializable]
+    public class Description{
+        public string FileName { get; set; }
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public short Rating { get; set; }
+        public string Comments { get; set; }
+        public List<string> ContributingArtists { get; set; }
+        public List<string> AlbumArtists { get; set; }
+        public string Album { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+
+        [Obsolete("Do not use this c'tor")]
+        public Description()
+        {
+            ContributingArtists = new List<string>();
+            AlbumArtists = new List<string>();
+        }
+
+        public Description(string fileName)
+        {
+            FileName = fileName;
+            ContributingArtists = new List<string>();
+            AlbumArtists = new List<string>();
+        }
+    }
+}
---
+++ DeepCopy/DeepCopy/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeepCopy
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DateTime now = DateTime.UtcNow;
+
+            MusicCollection mcoll = new MusicCollection("MyPhone", "Phone");
+
+            MusicFile mfile1 = new MusicFile("Brandenburg Concerto No. 1 in F Major.mp3");
+            mfile1.General.Accessed = now;
+            mfile1.General.Created = now.AddMonths(-1);
+            mfile1.Description.Album = "Bach - Brandenburg Collection";
+            mfile1.Description.Genre = "Baroque";
+
+            MusicFile mfile2 = new MusicFile("The Four Seasons.mp3");
+            mfile2.General.Accessed = now;
+            mfile2.General.Created = now.AddMonths(-2);
+            mfile2.Description.Album = "Antonio Vivaldi - The Four Seasons";
+            mfile2.Description.Genre = "Classical";
+
+            mcoll.AddMusic(mfile1);
+            mcoll.AddMusic(mfile2);
+
+            MusicCollection mcol2 = (MusicCollection)mcoll.Clone();
+            mcol2.DeviceName = "MyiPad";
+            mcol2.DeviceType = "Tablet";
+            mcol2.MusicFiles[1].Description.Genre = "Baroque";
+
+            Console.WriteLine(mcoll.MusicFiles[1].Description.Genre); // Returns Classical when deep-copied
+            Console.WriteLine(mcol2.MusicFiles[1].Description.Genre); // Returns Baroque
+        }
+    }
+}
